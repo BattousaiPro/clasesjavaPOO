4af620f4868a4dd345c56131fa3203f4cdf0f3ad
Jose Hernandez <jhernandez@kiosolutions.com>
2021-12-10 15:25:44 -0300

    mejoras funcnionalidad ejercicio tres

--- TrabajoExtra_Jorge_Saavedra/src/trabajoextra_jorge_saavedra/Ejercicio_Tres.java
+++ TrabajoExtra_Jorge_Saavedra/src/trabajoextra_jorge_saavedra/Ejercicio_Tres.java
@@ -4,18 +4,21 @@
  */
 package trabajoextra_jorge_saavedra;
 
+import java.util.ArrayList;
+import javax.swing.DefaultListModel;
+
 /**
  *
  * @author Batto
  */
 public class Ejercicio_Tres extends javax.swing.JFrame {
-    
- 
+
     /**
      * Creates new form Ejercicio_Tres
      */
     public Ejercicio_Tres() {
         initComponents();
+        jList1.setModel(modelo);
     }
 
     /**
@@ -34,10 +37,10 @@ public class Ejercicio_Tres extends javax.swing.JFrame {
         jList1 = new javax.swing.JList<>();
         jLabel3 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
-        jTextField3 = new javax.swing.JTextField();
         jButton4 = new javax.swing.JButton();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Ejercicio 3");
@@ -69,6 +72,11 @@ public class Ejercicio_Tres extends javax.swing.JFrame {
             }
         });
 
+        jLabel4.setFont(new java.awt.Font("Tahoma", 3, 12)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 0, 0));
+
+        jLabel6.setText("Resultado:");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -88,24 +96,24 @@ public class Ejercicio_Tres extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(32, 32, 32)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(18, 18, 18)
-                                .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(18, 18, 18)
-                                .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                        .addGap(32, 32, 32))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 182, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                                 .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 281, Short.MAX_VALUE)))
-                        .addContainerGap(32, Short.MAX_VALUE))))
+                        .addContainerGap(32, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                        .addGap(32, 32, 32))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -124,11 +132,11 @@ public class Ejercicio_Tres extends javax.swing.JFrame {
                 .addComponent(jButton4)
                 .addGap(16, 16, 16)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 16, Short.MAX_VALUE)
+                .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(16, 16, 16)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 24, Short.MAX_VALUE)
+                .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(16, 16, 16)
                 .addComponent(jButton1)
                 .addGap(24, 24, 24))
         );
@@ -143,8 +151,52 @@ public class Ejercicio_Tres extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        // TODO add your handling code here:
+        if (!ValidarCamposObligatorios()) {
+            String stg = jTextField1.getText().trim();
+            jTextField1.setText("");
+            listaNotas.add(stg);
+            modelo.removeAllElements();
+            for (int i = 0; i < listaNotas.size(); i++) {
+                modelo.addElement(listaNotas.get(i).toString().replace(".", ","));
+            }
+        }
+        calcularPromedio();
     }//GEN-LAST:event_jButton4ActionPerformed
+
+    public boolean ValidarCamposObligatorios() {
+        this.jLabel4.setText("");
+        this.jTextField1.setText(jTextField1.getText().trim());
+        boolean errPrecio = false;
+        if ("".equals(this.jTextField1.getText().trim())) {
+            this.jLabel4.setText("Campo Obligatorio.");
+            errPrecio = true;
+        } else {
+            try {
+                Double num = Double.parseDouble(this.jTextField1.getText().replace(",", ".").trim());
+                if (!(num >= 1 && num <= 7)) {
+                    this.jLabel4.setText("nota fuera de rango (1 a 7)");
+                    errPrecio = true;
+                }
+            } catch (NumberFormatException e) {
+                this.jLabel4.setText("Debe ser un número.");
+                errPrecio = true;
+            }
+        }
+        return errPrecio;
+    }
+
+    public void calcularPromedio() {
+        Double sumaPromedio = 0.0;
+        for (int i = 0; i < listaNotas.size(); i++) {
+            sumaPromedio = sumaPromedio + Double.parseDouble(listaNotas.get(i).toString().replace(",", "."));
+        }
+        Double promedio = sumaPromedio / listaNotas.size();
+        if (promedio < 4) {
+            jLabel5.setText("la suma es: " + sumaPromedio + " el promedio es: " + promedio + " Reprobo.");
+        } else {
+            jLabel5.setText("la suma es: " + sumaPromedio + " el promedio es: " + promedio + " Aprobo.");
+        }
+    }
 
     /**
      * @param args the command line arguments
@@ -181,6 +233,8 @@ public class Ejercicio_Tres extends javax.swing.JFrame {
         });
     }
 
+    private ArrayList listaNotas = new ArrayList();
+    private DefaultListModel modelo = new DefaultListModel();
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton4;
@@ -189,9 +243,9 @@ public class Ejercicio_Tres extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JList<String> jList1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField3;
     // End of variables declaration//GEN-END:variables
 }
