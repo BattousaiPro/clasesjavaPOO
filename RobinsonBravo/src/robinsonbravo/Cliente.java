d61254e80461c65e553ee2fd937033b734fd3708
BattousaiPro <BattousaiPro@outlook.com>
2021-12-14 16:35:27 -0300

    actualización de formularios

--- RobinsonBravo/src/robinsonbravo/Cliente.java
+++ RobinsonBravo/src/robinsonbravo/Cliente.java
@@ -1,21 +1,39 @@
 package robinsonbravo;
 
-public class Cliente {
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class Cliente extends Persona{
 
     private int idCliente;
     private int cargo;
     private int ciudad;
     private int tarjetasQueosee;
+    private int edad;
 
     public Cliente() {
     }
 
-    public Cliente(int idCliente, int cargo, int ciudad, int tarjetasQueosee) {
+    public Cliente(int idCliente, int cargo, int ciudad, int tarjetasQueosee, int edad, String rut, String nombre, String apellido, Date fecha_nacimiento, String estadoCivil, String sexo) {
+        super(rut, nombre, apellido, fecha_nacimiento, estadoCivil, sexo);
         this.idCliente = idCliente;
         this.cargo = cargo;
         this.ciudad = ciudad;
         this.tarjetasQueosee = tarjetasQueosee;
+        this.edad = edad;
     }
+
+    public Cliente(int idCliente, int cargo, int ciudad, int tarjetasQueosee, int edad) {
+        this.idCliente = idCliente;
+        this.cargo = cargo;
+        this.ciudad = ciudad;
+        this.tarjetasQueosee = tarjetasQueosee;
+        this.edad = edad;
+    }
+
+    
 
     public void setIdCliente(int idCliente) {
         this.idCliente = idCliente;
@@ -31,6 +49,14 @@ public class Cliente {
 
     public void setTarjetasQueosee(int tarjetasQueosee) {
         this.tarjetasQueosee = tarjetasQueosee;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getEdad() {
+        return edad;
     }
 
     public int getIdCliente() {
@@ -49,6 +75,19 @@ public class Cliente {
         return tarjetasQueosee;
     }
 
+    public void calcularEdad(String fecha) {
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        // super.getFecha_nacimiento()
+        LocalDate fechaNac = LocalDate.parse(fecha, fmt);
+        LocalDate ahora = LocalDate.now();
+
+        Period periodo = Period.between(fechaNac, ahora);
+        System.out.printf("Tu edad es: %s años, %s meses y %s días", periodo.getYears(), periodo.getMonths(), periodo.getDays());
+
+        this.edad = periodo.getYears();
+    }
+
     @Override
     public String toString() {
         return "Cliente{"
@@ -56,6 +95,7 @@ public class Cliente {
                 + ", cargo=" + cargo
                 + ", ciudad=" + ciudad
                 + ", tarjetasQueosee=" + tarjetasQueosee
+                + ", edad=" + edad
                 + '}';
     }
 
