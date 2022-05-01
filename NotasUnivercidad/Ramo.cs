e1be1c95b41e0557fb1164e97fb0f8b64969debd
Jose Hernandez <jhernandez@kiosolutions.com>
2022-04-30 22:51:04 -0400

    se re embosa codigo xD

--- NotasUnivercidad/Ramo.cs
+++ NotasUnivercidad/Ramo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace NotasUnivercidad
 {
@@ -62,5 +63,75 @@ namespace NotasUnivercidad
                 this.Promedio = prom / notas.Count;
             }
         }
+
+        public string[] arreglo()
+        {
+            string[] arregloRamo = new string[100];
+            try
+            {
+                StreamReader Lectura;  // Se declara la variable lectura de Tipo Lector de archivos
+                string cadena; // se declara la variable cadena que contendra los elementos del archivo
+                int contador = 1;  // contador de los elementos de la lista
+                Lectura = File.OpenText("Ramo.txt");  // lectura del archivo ramos.txt
+                cadena = Lectura.ReadLine();  // se lee la primera linea del archivo y se alamacena en la variable cadena
+                while (cadena != null)  //  ciclo repetitivo que recorre el archvo mientras su valor sea diferente a nulo
+                {
+                    Console.WriteLine(cadena + "----- " + (contador));  // se imprime en pantalla cada elemento del archivo
+                    arregloRamo[contador] = cadena;
+                    cadena = Lectura.ReadLine(); // se lee la siguiente linea del archivo
+
+                    contador++;  // se incrementa la posición de la lectura del archivo
+                }
+                Lectura.Close();  // se cierra el lector del archivo   
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            return arregloRamo;
+        }
+
+        public void borrar(string ramo)
+        {
+            try
+            {
+                StreamReader Lectura;
+                StreamWriter temporal;
+                string cadena;
+                bool encontrado = false;
+                Lectura = File.OpenText("Ramo.txt");
+                temporal = File.CreateText("temp.txt");
+                cadena = Lectura.ReadLine();
+                string respuesta = null;
+                while (cadena != null)
+                {
+                    if (cadena.Equals(ramo))
+                    {
+                        encontrado = true;
+                        Console.WriteLine("Ramo:" + cadena);
+                        respuesta = Console.ReadLine().ToUpper();
+                        if (!respuesta.Equals("SI"))
+                            temporal.WriteLine(cadena);
+                    }
+                    else
+                    {
+                        temporal.WriteLine(cadena);
+                    }
+                    cadena = Lectura.ReadLine();
+                }
+                if (encontrado == false)
+                    Lectura.Close();
+                temporal.Close();
+                File.Delete("Ramo.txt");
+                File.Move("temp.txt", "Ramo.txt");
+                Lectura.Close();
+                temporal.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+
     }
 }
