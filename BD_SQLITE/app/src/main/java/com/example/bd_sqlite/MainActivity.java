314ef3d30f24c0ca403770792eb3c41a814b8a83
Jose Hernandez <jhernandez@kiosolutions.com>
2022-11-17 22:21:43 -0300

    asaasas

--- BD_SQLITE/app/src/main/java/com/example/bd_sqlite/MainActivity.java
+++ BD_SQLITE/app/src/main/java/com/example/bd_sqlite/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.bd_sqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -13,16 +14,19 @@ public class MainActivity extends AppCompatActivity {
 
     public EditText user;
     public EditText pass;
+    public EditText textPrueba;
     public Button btnEntrar;
     public Button btnRegistrar;
     public BDUsuario bdUsuario;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         user = (EditText) findViewById(R.id.User);
         pass = (EditText) findViewById(R.id.RegApellido);
+        textPrueba = (EditText) findViewById(R.id.textoPrueba);
         btnEntrar = (Button) findViewById(R.id.btn_Entrar);
         btnRegistrar = (Button) findViewById(R.id.btn_Registrar);
         bdUsuario = new BDUsuario(this);
@@ -30,16 +34,21 @@ public class MainActivity extends AppCompatActivity {
         btnEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                textPrueba.setText("");
                 String u = user.getText().toString();
                 String p = pass.getText().toString();
                 if (u.equals("") && p.equals("")) {
+                    textPrueba.setText("elementos vacios");
                     Toast.makeText(getApplicationContext(), "ERROR : Campos Vacios", Toast.LENGTH_LONG).show();
                 } else if (bdUsuario.login(u, p) == 1) {
+                    textPrueba.setText("Login Okey");
                     Usuario us = bdUsuario.getUsuario(u, p);
                     Toast.makeText(getApplicationContext(), "Datos Correctos", Toast.LENGTH_LONG).show();
                     Intent i2 = new Intent(MainActivity.this, MainActivityMostrar.class);
                     i2.putExtra("id", us.getId());
                     startActivity(i2);
+                } else {
+                    textPrueba.setText("Else");
                 }
             }
         });
